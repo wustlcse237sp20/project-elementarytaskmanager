4a41446c34dfc86cfda967cf0d4deec7876f100e
Jessica Thoelke <j.thoelke@wustl.edu>
2020-04-05 21:21:56 -0500

    added initial Task class

---
+++ src/Task.java
@@ -0,0 +1,33 @@
+
+public class Task {
+	String name;
+	String category;
+	
+	public Task(String name, String category) {
+		this.name = name;
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return name + "  " + category;
+	}
+	
+	
+}
