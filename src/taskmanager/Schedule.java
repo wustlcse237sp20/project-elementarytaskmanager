94e368af8cb8c80e64ca64a277631a3ee160ed6f
EmilyLopez-Ichikawa <e.lopez-ichikawa@wustl.edu>
2020-04-06 08:52:14 -0500

    creating Schedule class to make Student class smaller and easier to read

---
+++ src/taskmanager/Schedule.java
@@ -0,0 +1,21 @@
+package taskmanager;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Schedule {
+	private File file;
+	
+	public Schedule() {
+		
+	}
+	
+	public void addTask(Task task) {
+		
+	}
+	
+	public List<Task> getTasks(){
+		return new LinkedList<Task>();
+	}
+}
