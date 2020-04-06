a781b612d6d1c3362c1ceb7a14e5d32b463124f3
EmilyLopez-Ichikawa <e.lopez-ichikawa@wustl.edu>
2020-04-06 00:24:07 -0500

    added bash script to compile all classes and run either student or teacher class based on user input

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd src
+
+javac ./taskmanager/Student.java
+javac ./taskmanager/Teacher.java
+javac ./taskmanager/Task.java
+
+echo "Are you a teacher? (y/n)"
+read ISTEACHER
+
+if [[ $ISTEACHER == "y" || $ISTEACHER == "Y" ]]
+then
+	java taskmanager/Teacher
+else
+	java taskmanager/Student
+fi
+
+rm ./taskmanager/Student.class
+rm ./taskmanager/Teacher.class
+rm ./taskmanager/Task.class
