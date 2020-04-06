67183a460a1dfbc50c8a11cc837135a61c5ea514
speiffer@wustl.edu <speiffer@wustl.edu>
2020-04-06 00:49:27 -0500

    Added first test file

--- src/tests/StudentTest.java
+++ src/tests/StudentTest.java
@@ -1,5 +1,28 @@
 package tests;
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+import taskmanager.Student;
+import taskmanager.Teacher;
+import taskmanager.Task;
 
 public class StudentTest {
+
+			
+			@Test
+			public void addSingleTask() throws Exception{
+				Student toAddTask = new Student(new File("toAddTask"), "toAddTask");
+				//toAddTask.addTask(task);
+				assertTrue(true);
+
+			}
+
+			
+
+		
+
+	
 
 }
