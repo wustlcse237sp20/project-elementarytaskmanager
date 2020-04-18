76665b7be002d8e7542702fd2c39c873f8d0db2e
Jessica Thoelke <j.thoelke@wustl.edu>
2020-04-17 20:22:39 -0500

    updated window layout

--- src/gui/TasksWindow.java
+++ src/gui/TasksWindow.java
@@ -12,6 +12,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JList;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
 
 public class TasksWindow {
 
@@ -49,8 +51,6 @@ public class TasksWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		SpringLayout springLayout = new SpringLayout();
-		frame.getContentPane().setLayout(springLayout);
 		
 		if(isTeacher) {
 			controller = new TeacherController();
@@ -64,30 +64,26 @@ public class TasksWindow {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		springLayout.putConstraint(SpringLayout.SOUTH, btnNewButton, -10, SpringLayout.SOUTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, btnNewButton, -10, SpringLayout.EAST, frame.getContentPane());
-		frame.getContentPane().add(btnNewButton);
+		frame.getContentPane().setLayout(new MigLayout("", "[133px][133px][133px][right]", "[][243px]"));
+		
+		JLabel lblNewLabel = new JLabel("To Do");
+		frame.getContentPane().add(lblNewLabel, "cell 0 0");
+		
+		JLabel lblNewLabel_1 = new JLabel("In Progress");
+		frame.getContentPane().add(lblNewLabel_1, "cell 1 0");
+		
+		JLabel lblNewLabel_2 = new JLabel("Done");
+		frame.getContentPane().add(lblNewLabel_2, "cell 2 0");
+		frame.getContentPane().add(btnNewButton, "cell 3 1,alignx right,aligny bottom");
 		
 //		JList<Task> toDoList = new JList(controller.getToDoTasks());
 		JList<Task> toDoList = new JList(controller.getToDoTasks());
-		springLayout.putConstraint(SpringLayout.NORTH, toDoList, 25, SpringLayout.NORTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, toDoList, 30, SpringLayout.WEST, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, toDoList, 0, SpringLayout.SOUTH, btnNewButton);
-		springLayout.putConstraint(SpringLayout.EAST, toDoList, -287, SpringLayout.EAST, frame.getContentPane());
-		frame.getContentPane().add(toDoList);
+		frame.getContentPane().add(toDoList, "cell 0 1,grow");
 		
 		JList<Task> inProgressList = new JList<Task>();
-		springLayout.putConstraint(SpringLayout.NORTH, inProgressList, 31, SpringLayout.NORTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, inProgressList, 10, SpringLayout.EAST, toDoList);
-		springLayout.putConstraint(SpringLayout.SOUTH, inProgressList, 0, SpringLayout.SOUTH, btnNewButton);
-		frame.getContentPane().add(inProgressList);
+		frame.getContentPane().add(inProgressList, "cell 1 1,grow");
 		
 		JList<Task> doneList = new JList<Task>();
-		springLayout.putConstraint(SpringLayout.EAST, inProgressList, -62, SpringLayout.WEST, doneList);
-		springLayout.putConstraint(SpringLayout.NORTH, doneList, 25, SpringLayout.NORTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, doneList, 273, SpringLayout.WEST, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, doneList, 0, SpringLayout.SOUTH, btnNewButton);
-		springLayout.putConstraint(SpringLayout.EAST, doneList, -34, SpringLayout.WEST, btnNewButton);
-		frame.getContentPane().add(doneList);
+		frame.getContentPane().add(doneList, "cell 2 1,grow");
 	}
 }
