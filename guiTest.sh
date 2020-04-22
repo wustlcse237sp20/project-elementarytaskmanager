c90ac90a424da2ae0c247f01d2141ceab8f7ca8d
Jessica Thoelke <j.thoelke@wustl.edu>
2020-04-22 14:22:54 -0500

    updated script, works for macs

--- guiTest.sh
+++ guiTest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-javac -cp miglayout15-swing.jar  ./src/taskmanager/*.java ./src/gui/*.java -d bin/
-java -cp bin/ gui/TasksWindow
+javac -cp miglayout15-swing.jar ./src/taskmanager/*.java ./src/gui/*.java -d bin/
+java -cp "miglayout15-swing.jar:bin/" gui/TasksWindow
 
 rm ./bin/gui/*class
 rm ./bin/taskmanager/*class
-
